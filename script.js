b3224b428786f8ae373c00a1922edbe9c323cfcb
MailsonAndradeCardoso <mailsoncardoso1988@gmail.com>
2022-10-03 16:01:33 -0300

    Exercicios ok

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//Ex 01
 const frutas = ["laranja", "limão", "uva"];
+
+const fruta1 = document.getElementById("fruta-1")
+fruta1.innerHTML += frutas[0]
+
+const fruta2 = document.getElementById("fruta-2")
+fruta2.innerHTML += frutas[1]
+
+const fruta3 = document.getElementById("fruta-3")
+fruta3.innerHTML += frutas[2]
+
+//EX 02
+function frutaImput(){document.getElementById("fruta")
+console.log(frutaImput.value)
+
+//EX 03
+
+document.getElementById("fruta-4").innerHTML = frutaImput.value}
